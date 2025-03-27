38df2f0323341825f29d724b664087f63e8865a2
DiegoLinares11 <lin221256@uvg.edu.gt>
2025-03-27 16:54:39 -0600

    match handler

---
+++ backend/internal/handlers/match_handler.go
@@ -0,0 +1,175 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/DiegoLinares11/Lab-6-Backend-Only/internal/models"
+	"github.com/DiegoLinares11/Lab-6-Backend-Only/internal/storage"
+	"github.com/gorilla/mux"
+)
+
+type MatchHandler struct {
+	storage *storage.PostgresStorage
+}
+
+func NewMatchHandler(ps *storage.PostgresStorage) *MatchHandler {
+	return &MatchHandler{storage: ps}
+}
+
+func (mh *MatchHandler) GetAllMatches(w http.ResponseWriter, r *http.Request) {
+	matches, err := mh.storage.GetAllMatches()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(matches)
+}
+
+func (mh *MatchHandler) GetMatchByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid match ID", http.StatusBadRequest)
+		return
+	}
+
+	match, err := mh.storage.GetMatchByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(match)
+}
+
+func (mh *MatchHandler) CreateMatch(w http.ResponseWriter, r *http.Request) {
+	var match models.Match
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&match); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	match.MatchDate = match.MatchDate.UTC()
+	if err := mh.storage.CreateMatch(&match); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(match)
+}
+
+func (mh *MatchHandler) UpdateMatch(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid match ID", http.StatusBadRequest)
+		return
+	}
+
+	var match models.Match
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&match); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	match.ID = id
+	match.MatchDate = match.MatchDate.UTC()
+	if err := mh.storage.UpdateMatch(&match); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(match)
+}
+
+func (mh *MatchHandler) DeleteMatch(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid match ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := mh.storage.DeleteMatch(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (mh *MatchHandler) RegisterGoal(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid match ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := mh.storage.RegisterGoal(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (mh *MatchHandler) RegisterYellowCard(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid match ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := mh.storage.RegisterYellowCard(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (mh *MatchHandler) RegisterRedCard(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid match ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := mh.storage.RegisterRedCard(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (mh *MatchHandler) SetExtraTime(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid match ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := mh.storage.SetExtraTime(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
